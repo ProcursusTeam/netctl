5ace600793787bc6c19ed3bb03e1845698cb156f
Cameron Katri <me@cameronkatri.com>
2022-02-03 21:17:26 -0500

    netctl airplane: Don't segfault when setting

--- airplane/airplane.m
+++ airplane/airplane.m
@@ -1,31 +1,46 @@
+#import <CoreFoundation/CoreFoundation.h>
 #import <Foundation/Foundation.h>
+
+@protocol RadiosPreferencesDelegate <NSObject>
+-(void)airplaneModeChanged;
+@end
 
 @interface RadiosPreferences : NSObject
 @property (nonatomic) bool airplaneMode;
-- (void)refresh;
+@property (nonatomic) id <RadiosPreferencesDelegate> delegate;
+-(void)refresh;
+-(void)syncrohnize;
+@end
+
+@interface NCRadiosPreferences : RadiosPreferences<RadiosPreferencesDelegate>
+@end
+
+@implementation NCRadiosPreferences
+-(void)airplaneModeChanged {
+	CFRunLoopStop(CFRunLoopGetCurrent());
+}
 @end
 
 int airplane(char *set) {
-	RadiosPreferences *radiosPreferences = [RadiosPreferences new];
+	NCRadiosPreferences *radiosPreferences = [NCRadiosPreferences new];
 
-	[radiosPreferences refresh];
+	radiosPreferences.delegate = radiosPreferences;
 
 	if (set == NULL || !strcmp(set, "status")) {
 		printf("%s\n", [radiosPreferences airplaneMode] ? "on" : "off");
 		return 0;
 	} else if (!strcmp(set, "on")) {
 		[radiosPreferences setAirplaneMode:1];
-		return 0;
 	} else if (!strcmp(set, "off")) {
 		[radiosPreferences setAirplaneMode:0];
-		return 0;
 	} else if (!strcmp(set, "toggle")) {
 		[radiosPreferences setAirplaneMode:![radiosPreferences airplaneMode]];
-		return 0;
 	} else {
 		fprintf(stderr, "Usage: netctl airplane [status | toggle | on | off]\n");
 		return 1;
 	}
 
-	return 1;
+	CFRunLoopRun();
+
+	return 0;
 }
