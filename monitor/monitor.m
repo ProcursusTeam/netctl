4770e00654f766a93b7048ca57172f36dfe262b4
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2022-02-18 21:47:45 -0600

    basic argument checking

--- monitor/monitor.m
+++ monitor/monitor.m
@@ -1,3 +1,5 @@
+#include <err.h>
+
 #include <Foundation/Foundation.h>
 #import <NetworkStatistics/NetworkStatistics.h>
 
@@ -15,7 +17,31 @@ void(^callback)(void*, void*) = ^(NStatSourceRef ref, void* arg2) {
 };
 
 int nctl_monitor(int argc, char** argv) {
+    if (argc < 2) {
+        errno = EINVAL;
+        errx(1, "not enough args");
+    }
+
+    BOOL monitorTCP = NO;
+    BOOL monitorUDP = NO;
+
+    if (!strcmp(argv[1], "tcp")) { monitorTCP = YES; }
+    if (!strcmp(argv[1], "udp")) { monitorUDP = YES; }
+    if (!strcmp(argv[1], "all")) {
+        monitorTCP = YES;
+        monitorUDP = YES;
+    }
+
     NStatManagerRef ref = NStatManagerCreate(kCFAllocatorDefault, dispatch_get_main_queue(), callback);
+
+    if (monitorTCP) {
+        NStatManagerAddAllTCPWithFilter(ref, 0, 0);
+    }
+
+    if (monitorUDP) {
+        NStatManagerAddAllUDPWithFilter(ref, 0, 0);
+    }
+
     NStatManagerSetFlags(ref, 0);
 
     NStatManagerAddAllTCPWithFilter(ref, 0, 0);
